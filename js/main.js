90caebb05a579a035b60981d37cb80afc976061e
lucasantos-S <lucassantos.dsilv@gmail.com>
2021-10-19 15:06:08 -0300

    inclusao da animação/js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const sr = ScrollReveal({
+        origin: 'top',
+        distance: '50px',
+        duration: 1500,
+        reset: false
+     });
+    
+    ScrollReveal().reveal('.info', { delay: 200 });
+    ScrollReveal().reveal('.btn', { delay: 400 });
+    ScrollReveal().reveal('.wallpaper', { delay: 600 });
+    ScrollReveal().reveal('.contend', { delay: 800});
+    ScrollReveal().reveal('.form', { delay: 1000});
